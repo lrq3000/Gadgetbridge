346ad353a76397673ea587180f3350417a839a3c
Arjan Schrijver <a_gadgetbridge@anymore.nl>
2023-05-15 23:09:02 +0200

    Add simple dashboard view

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/MainActivity.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/MainActivity.java
@@ -48,6 +48,7 @@ import androidx.fragment.app.DialogFragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.color.DynamicColors;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -138,12 +139,13 @@ public class MainActivity extends AbstractGBActivity implements BottomNavigation
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        DynamicColors.applyToActivitiesIfAvailable(getApplication());
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottom_nav_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         // TODO: read last view from savedInstanceState
-        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_devices);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_dashboard);
 
         IntentFilter filterLocal = new IntentFilter();
         filterLocal.addAction(GBApplication.ACTION_LANGUAGE_CHANGE);
